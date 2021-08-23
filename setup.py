8d9d51f2124b63fea329a97e0f838a9934659df7
rohanricky0609 <rohan.kothapalli@clouderizer.com>
2021-08-23 14:42:52 +0530

    test in colab

--- setup.py
+++ setup.py
@@ -7,21 +7,28 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 cmds_to_run = [
-    "mkdir /home/rohan/model_data",
-    "wget -O /home/rohan/model_data/answers_vqa.txt https://dl.fbaipublicfiles.com/pythia/data/answers_vqa.txt",
-    "wget -O /home/rohan/model_data/vocabulary_100k.txt https://dl.fbaipublicfiles.com/pythia/data/vocabulary_100k.txt",
-    "wget -O /home/rohan/model_data/detectron_model.pth  https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.pth", 
-    "wget -O /home/rohan/model_data/pythia.pth https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.pth",
-    "wget -O /home/rohan/model_data/pythia.yaml https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.yml",
-    "wget -O /home/rohan/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml",
-    "wget -O /home/rohan/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz",
-    "tar xf /home/rohan/model_data/detectron_weights.tar.gz"
+    "mkdir /content/model_data",
+    "wget -O /content/model_data/answers_vqa.txt https://dl.fbaipublicfiles.com/pythia/data/answers_vqa.txt",
+    "wget -O /content/model_data/vocabulary_100k.txt https://dl.fbaipublicfiles.com/pythia/data/vocabulary_100k.txt",
+    "wget -O /content/model_data/detectron_model.pth  https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.pth", 
+    "wget -O /content/model_data/pythia.pth https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.pth",
+    "wget -O /content/model_data/pythia.yaml https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.yml",
+    "wget -O /content/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml",
+    "wget -O /content/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz",
+    "tar xf /content/model_data/detectron_weights.tar.gz",
+    "cd /content/",
+    "git clone https://github.com/facebookresearch/mmf.git mmf && cd mmf",
+    "sed -i '/torch/d' requirements.txt && pip install -e .",
+    "cd /content && git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git && cd vqa-maskrcnn-benchmark",
+    "python setup.py build && python setup.py develop",
 ]
 
 class PostInstallCommand(install):
     def run(self):
         for cmd in cmds_to_run:
             check_call(cmd.split())
+
+        from mmf_demo import MMFDemo
 
 setuptools.setup(
     name="example-pkg-YOUR-USERNAME-HERE",

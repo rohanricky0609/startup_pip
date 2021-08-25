a9a15531d454470a3cfdbeb2cc9161bbbc14f906
rohanricky0609 <rohan.kothapalli@clouderizer.com>
2021-08-25 10:35:47 +0530

    vfbx

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ cmds_to_run = [
     "wget -O /content/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml",
     "wget -O /content/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz",
     "tar xf /content/model_data/detectron_weights.tar.gz",
-    "cd /content/ && git clone https://github.com/facebookresearch/mmf.git mmf",
-    "cd /content/mmf && sed -i '/torch/d' requirements.txt && pip install -e .",
-    "cd /content && git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git",
-    "cd vqa-maskrcnn-benchmark && python setup.py build && python setup.py develop",
+    "git clone https://github.com/facebookresearch/mmf.git /content/mmf",
+    "sed -i '/torch/d' requirements.txt && pip install -e /content/mmf",
+    "git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git",
+    "cd /content/vqa-maskrcnn-benchmark && python setup.py build && python setup.py develop",
 ]
 
 # "cd /home/rohan/",

ee5437cd6c8847d8feb382e9badc3b67940e7246
rohanricky0609 <rohan.kothapalli@clouderizer.com>
2021-09-01 10:30:47 +0530

    fdd

--- setup.py
+++ setup.py
@@ -8,28 +8,40 @@ with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
 cmds_to_run = [
-	"mkdir -p /content/model_data",
-	"wget -O /content/model_data/answers_vqa.txt https://dl.fbaipublicfiles.com/pythia/data/answers_vqa.txt",
-	"wget -O /content/model_data/vocabulary_100k.txt https://dl.fbaipublicfiles.com/pythia/data/vocabulary_100k.txt",
-	"wget -O /content/model_data/detectron_model.pth  https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.pth", 
-	"wget -O /content/model_data/pythia.pth https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.pth",
-	"wget -O /content/model_data/pythia.yaml https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.yml",
-	"wget -O /content/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml",
-	"wget -O /content/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz",
-	"tar xf /content/model_data/detectron_weights.tar.gz",
-	"git clone https://github.com/facebookresearch/mmf.git /content/dmmf",
-	"sed -i /torch/d /content/dmmf/requirements.txt",
-	"pip install -e /content/dmmf",
-	"git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git /content/vqa-maskrcnn-benchmark",
-	"python /content/vqa-maskrcnn-benchmark/setup.py build",
-	"cd /content/vqa-maskrcnn-benchmark && python setup.py develop",
-	"mkdir -p /content/done"
+	"apt-get update",
+	"apt-get install ffmpeg libsm6 libxext6  -y",
+	"mkdir -p /home/app/model_data",
+	"wget -O /home/app/model_data/answers_vqa.txt https://dl.fbaipublicfiles.com/pythia/data/answers_vqa.txt",
+	"wget -O /home/app/model_data/vocabulary_100k.txt https://dl.fbaipublicfiles.com/pythia/data/vocabulary_100k.txt",
+	"wget -O /home/app/model_data/detectron_model.pth  https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.pth", 
+	"wget -O /home/app/model_data/pythia.pth https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.pth",
+	"wget -O /home/app/model_data/pythia.yaml https://dl.fbaipublicfiles.com/pythia/pretrained_models/vqa2/pythia_train_val.yml",
+	"wget -O /home/app/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml",
+	"wget -O /home/app/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz",
+	"tar xf /home/app/model_data/detectron_weights.tar.gz",
+	"git clone https://github.com/facebookresearch/mmf.git /home/app/dmmf",
+	"sed -i /torch/d /home/app/dmmf/requirements.txt",
+	"pip install -e /home/app/dmmf",
+	"git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git /home/app/vqa-maskrcnn-benchmark",
+	"python /home/app/vqa-maskrcnn-benchmark/setup.py build",
+	"cd /home/app/vqa-maskrcnn-benchmark && python setup.py develop",
+	"mkdir -p /home/app/done"
 ]
 
-# "cd /home/rohan/",
+cmds_to_run_local = [
+	"git clone https://github.com/facebookresearch/mmf.git /home/app/dmmf",
+	"sed -i /torch/d /home/app/dmmf/requirements.txt",
+	"pip install -e /home/app/dmmf",
+	"git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git /home/app/vqa-maskrcnn-benchmark",
+	"python3 /home/app/vqa-maskrcnn-benchmark/setup.py build",
+	"cd /home/app/vqa-maskrcnn-benchmark && python setup.py develop",
+	"mkdir -p /home/app/done"
+]
+
+# "cd /home/app/",
 #     "git clone https://github.com/facebookresearch/mmf.git mmf && cd mmf",
 #     "sed -i '/torch/d' requirements.txt && pip install -e .",
-#     "cd /home/rohan && git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git && cd vqa-maskrcnn-benchmark",
+#     "cd /home/app && git clone https://github.com/rohanricky0609/vqa-maskrcnn-benchmark.git && cd vqa-maskrcnn-benchmark",
 #     "python setup.py build && python setup.py develop",
 
 class PostInstallCommand(install):
